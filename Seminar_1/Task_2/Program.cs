195be444ed314d106aba9f33164bbf337447af4e
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-23 17:46:15 +0300

    Task_2

---
+++ Seminar_1/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// task 2
+Console.WriteLine($"Input first number ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Input second number ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if ( n1 > n2 ) 
+{
+    Console.WriteLine($"{n1} - is the biggest number, {n2} - is the smallest.");
+}
+else 
+{
+    Console.WriteLine($"{n2} - is the biggest number, {n1} - is the smallest.");
+}
