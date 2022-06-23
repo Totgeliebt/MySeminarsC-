e301167868ab6ba47427a891319f7601f175a83d
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-23 18:21:23 +0300

    Task_8

---
+++ Seminar_1/Task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine($"Input a number ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int even = 1;
+while (even <= N)
+{
+    if(even % 2 == 0){
+        Console.WriteLine($"{even} ");
+    }
+    even++;
+}
