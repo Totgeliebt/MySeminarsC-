b51acd857f9a96411c3071155c416a73a239caec
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-23 18:04:00 +0300

    Task_4

---
+++ Seminar_1/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine($"Input first number ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Input second number ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Input third number ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if (n2 > n1)  max = n2;
+if (n3 > n2)  max = n3;
+
+Console.WriteLine(max);
