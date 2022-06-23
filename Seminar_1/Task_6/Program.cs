75193e86eacf97e816ef0247dd00b6fcc6cfd031
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-23 18:07:50 +0300

    Task_6

---
+++ Seminar_1/Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine($"Input a number ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+if( n1 % 2 == 0) 
+{
+    Console.WriteLine($"{n1} - even");
+}
+else
+{
+    Console.WriteLine($"{n1} - odd");
+}
