40f209c6e00bdc9746717f5b809ad95dfa912974
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-24 22:11:25 +0300

    Task 15

---
+++ Seminar_2/HW/Task_15/Program.cs
@@ -0,0 +1,11 @@
+﻿
+void CheckTheDayOfTheWeek (int DayOfTheWeek) {
+  if (DayOfTheWeek == 6 || DayOfTheWeek == 7) {
+  Console.WriteLine("Hurray! It's the weekend!");
+  }
+  else Console.WriteLine("Argh! It's time to work hard!");
+}
+Console.Write("Input the number of the day of the week from 1 to 7 ");
+int DayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+
+CheckTheDayOfTheWeek(DayOfTheWeek);
