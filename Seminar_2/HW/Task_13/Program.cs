55f4d1fb648f0d1aed292471cf7d35e432496e56
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-24 21:42:59 +0300

    Task 13

---
+++ Seminar_2/HW/Task_13/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int ThirdDigit(int num) 
+{
+  string numText = Convert.ToString(num);
+    if (numText.Length > 2){
+  Console.WriteLine("третья цифра -> " + numText[2]);
+  return num;
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+  return 0;
+}
+}
+
+Console.WriteLine($"Input the natural number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int res = ThirdDigit(num);
+if(res!= 0) Console.WriteLine($"The third digit is {res}");
