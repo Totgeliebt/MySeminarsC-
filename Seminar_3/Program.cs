bbf85dff06b9e62002aa66cc249cdc0d012a2607
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-29 19:36:41 +0300

    Task 19

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+            int CheckPalindrom(int str)
+            {
+            int prov = 0;
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str.Substring(i, 1) != (str.Substring(str.Length - 1 - i, 1)))
+                {
+                    Console.WriteLine($"Число {str} не является палиндромом");
+                    break;
+                }
+                else
+                    prov = 1;
+            }
+            if (prov == 1) Console.WriteLine($"Число {str} является палиндромом");
+            }
+
+             Console.WriteLine("Введите число: ");
+            string str = Convert.ToInt32(Console.ReadLine());
---
+++ Seminar_3/Task_19/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+void IsPalindrome(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число {number} - НЕ палиндром.");
+}
+
+Console.WriteLine("Input number 5-digit number: ");
+string Num = Console.ReadLine();
+ IsPalindrome(Num);
+
