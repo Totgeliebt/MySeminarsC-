66fc5e52fbe94dfd5d49e3583cb36c8ab3f595d3
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-30 15:56:25 +0300

    Task_21

--- Seminar_3/Task_21/Program.cs
+++ Seminar_3/Task_21/Program.cs
@@ -6,21 +6,9 @@ double GetDistance(double x1, double y1, double z1, double x2,  double y2,  doub
     double zLine = z2-z1;
 
     double distQuad = xLine *xLine + yLine*yLine + zLine*zLine;
-    double result = Math.Sqrt(distQuad);
+    double result = Math.Round(Math.Sqrt(distQuad), 2);
+    Console.WriteLine(result);
     return result;
 }
 
-Console.WriteLine("Input number: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
-double result = GetDistance(x1,y1,z1,x2,y2,z2); 
-// double result = GetDistance(3,6,8,2,1,-7); 
+double result = GetDistance(3,6,8,2,1,-7); 
