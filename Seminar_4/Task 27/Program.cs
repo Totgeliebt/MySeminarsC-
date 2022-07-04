fa9674d1b695558b3a4c674f9fd082836a459831
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-07-04 17:58:07 +0300

    Task 27

---
+++ Seminar_4/Task 27/Program.cs
@@ -0,0 +1,16 @@
+﻿int FindSum(int num)
+{
+    int sum = 0;
+    int digit = 1;
+    while(digit != 0){
+        digit = num %10;
+        sum+=digit;
+        num = num/10;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindSum(num));
