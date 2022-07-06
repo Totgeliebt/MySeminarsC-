7541ff9f4d66c672e901c154383480c617405754
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-07-06 16:40:26 +0300

    Task 29

---
+++ Seminar_4/Task 29/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+
+int[] CreateSomeArray(int size)
+
+{
+    int [] array = new int [size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(0,1000);
+
+    return array;
+}
+
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+
+    Console.WriteLine();
+}
+
+int[] SomeArray = CreateSomeArray(8);
+ShowArray(SomeArray);
