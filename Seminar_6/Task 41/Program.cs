d944f3d80c454597d36bf2ab35764c38d9c17b63
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-07-08 21:13:28 +0300

    Task 41 and 43

---
+++ Seminar_6/Task 41/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[] CreateSomeArray(int size)
+
+{
+    int [] array = new int [size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-1000,1000);
+
+    return array;
+}
+
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+
+    Console.WriteLine();
+}
+
+int HowManyPositive(int M, int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < M; i++)
+    { 
+        if( array[i] > 0) count++;
+    }
+    
+        return count;
+}
+
+Console.WriteLine("Input number: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] SomeArray = CreateSomeArray(M);
+ShowArray(SomeArray);
+Console.WriteLine(HowManyPositive(M,SomeArray));
---
+++ Seminar_6/Task 43/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+double FindX(int b1, int k1, int b2, int k2)
+{
+    double x;
+    return x = Math.Round((double)(b2-b1)/(k1-k2), 1);
+}
+
+double FindY(int b1, int k1, double x)
+{
+    double y;
+    return y = Math.Round((double)(k1*x + b1), 1);
+}
+
+Console.WriteLine("Input b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+double x = FindX(b1, k1, b2, k2);
+double y = FindY(b1, k1, x);
+Console.WriteLine($"Координаты точки пересечения: ({x};{y})");
