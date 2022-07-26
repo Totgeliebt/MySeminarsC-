88f6258547f18a76505a45eeda978e099500d66a
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-07-26 18:45:19 +0300

    Seminar 10 HW

---
+++ Seminar_10/Task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string[] CreateStringArray(int number) 
+{
+    string[] newArray = new string[number];
+    for( int i = 0; i < number; i++ ) {
+        Console.WriteLine($"input {i+1} word: ");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
+int CountVowelWords(string[] array)
+{       int count = 0;
+     for (int i = 0; i < array.Length; i++)
+    {
+        if( array[i][0] == 'a' || array[i][0] == 'e' || array[i][0] == 'i' || array[i][0] == 'o' || array[i][0] == 'u') count++;
+    }
+    return count;
+}
+string[] myArray = CreateStringArray(5);
+Console.WriteLine("Word(s) started with vowels - "+CountVowelWords(myArray));
---
+++ Seminar_10/Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿
+string[] CreateStringArray(int number) 
+{
+    string[] newArray = new string[number];
+    for( int i = 0; i < number; i++ ) {
+        Console.WriteLine($"input {i+1} word: ");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
+string[] CreateDoubleStringArray(string[] array) 
+{
+    string[] newArray = new string[array.Length/2];
+    if(array.Length%2 ==1) {
+        return array;
+    } 
+    else
+    {
+        for (int i = 0, j = 0; i < array.Length; i += 2, j++)
+         {
+            newArray[j]=array[i]+array[i+1];
+        } 
+    return newArray;
+    }
+}
+void ShowStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+string[] myArray = CreateStringArray(4);
+ShowStringArray(CreateDoubleStringArray(myArray));
